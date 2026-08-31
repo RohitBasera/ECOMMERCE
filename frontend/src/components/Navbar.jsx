5e5d9bc736a288fa188ea9b4d950835f287ed66a
Rohit Basera <rudrsingh2004@gmail.com>
2026-08-31 06:56:24 -0700

    Phase 1.1

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -30,7 +30,14 @@ const Navbar = () => {
       <div className='flex items-center gap-6'>
         < img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
         <div className='group relative'>
-          <img src={assets.profile_icon} alt="" />
+          <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+        <div className='group-hover:block hidden absolute dropdown-menu right-o pt-4'>
+            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+              <p className='cursor-pinter hover:text-black'>My Profile</p>
+              <p className='cursor-pinter hover:text-black'>Orders</p>
+              <p className='cursor-pinter hover:text-black'>Logout</p>
+            </div>
+        </div>
         </div>
 
       </div>
