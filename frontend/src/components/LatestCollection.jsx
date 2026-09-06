06c186d41596b065112194f2b0b7211c73954358
Rohit Basera <rudrsingh2004@gmail.com>
2026-09-06 19:20:41 +0530

    Product Placement

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,19 +1,33 @@
 import React, { useContext } from 'react'
-import {ShopContext} from '../context/ShopContext'
+import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
+import ProductItem from './Productitem'
 
 const LatestCollection = () => {
 
     const { products } = useContext(ShopContext);
-       
-  
-  
+    const latestProducts = products.slice(0, 10);
+
     return (
     <div className='my-10'>
       <div className='text-center py-8 text-3xl'>
-        <Title text1={'Latest'} text2={'   Collection'}/>
+        <Title text1={'Latest'} text2={'   Collection'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Discover our newest arrivals and exclusive pieces
+        </p>
       </div>
-      
+
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
     </div>
   )
 }
